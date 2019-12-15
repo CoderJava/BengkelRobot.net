7a97d61b0242da38906275dc532cc1982a338785
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-12-15 22:23:43 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Welcome to Bengkel Robot',
+          style: Theme.of(context).textTheme.display1,
+        ),
+      ),
+    );
+  }
+}
