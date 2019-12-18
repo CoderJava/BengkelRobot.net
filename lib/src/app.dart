fde43f4f2ab39c737a1f0c1ccf26b9d4eaa50c26
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-12-19 00:21:44 +0700

    Created prototype for showing latest post articles

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,6 +6,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Bengkel Robot',
       home: HomeScreen(),
     );
   }
--- lib/src/ui/home/home_screen.dart
+++ lib/src/ui/home/home_screen.dart
@@ -1,82 +1,228 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+// ignore: must_be_immutable
 class HomeScreen extends StatelessWidget {
   final double paddingScreen = 32.0;
+  final List<String> listImages = [
+    'assets/images/img_content_sample_1.jpg',
+    'assets/images/img_content_sample_2.jpg',
+    'assets/images/img_content_sample_3.jpg',
+    'assets/images/img_content_sample_4.jpg',
+    'assets/images/img_content_sample_5.jpg',
+    'assets/images/img_content_sample_6.jpg',
+  ];
+  double widthScreen;
+  double heightScreen;
+  int counterItemLatestPost = 0;
 
   @override
   Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    widthScreen = mediaQueryData.size.width;
+    heightScreen = mediaQueryData.size.height;
+
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
+      body: ListView(
         children: <Widget>[
-          _buildWidgetHeader(context),
+          _buildWidgetWelcome(context),
+          _buildWidgetSizedBox(height: paddingScreen),
+          Padding(
+            padding: EdgeInsets.only(left: paddingScreen + 16.0),
+            child: Text(
+              'Latest Post',
+              style: Theme.of(context).textTheme.headline,
+            ),
+          ),
+          _buildWidgetSizedBox(height: 16.0),
+          GridView.count(
+            padding: EdgeInsets.symmetric(horizontal: paddingScreen),
+            shrinkWrap: true,
+            crossAxisCount: 3,
+            childAspectRatio: 1.0,
+            children: List.generate(
+              listImages.length,
+              (index) {
+                counterItemLatestPost += 1;
+                if (counterItemLatestPost == 4) {
+                  counterItemLatestPost = 1;
+                }
+                String image = listImages[index];
+                return Padding(
+                  padding: EdgeInsets.only(
+                    left: 16.0,
+                    right: 16.0,
+                    bottom: paddingScreen,
+                  ),
+                  child: Card(
+                    elevation: 4.0,
+                    child: Column(
+                      children: <Widget>[
+                        _buildWidgetImageCoverItemPost(image),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              _buildWidgetCaptionItemPost(),
+                              Flexible(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                    left: 16.0,
+                                    top: 8.0,
+                                    right: 16.0,
+                                  ),
+                                  child: Text(
+                                    'How to Find the Video Games of Your Youth',
+                                    style: Theme.of(context).textTheme.title,
+                                    maxLines: 2,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
   }
 
-  Widget _buildWidgetHeader(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      padding: EdgeInsets.only(left: paddingScreen, top: paddingScreen, right: paddingScreen, bottom: 16.0,),
+  Widget _buildWidgetCaptionItemPost() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16.0,
+        top: 16.0,
+      ),
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Text(
-            'BengkelRobot',
-            style: Theme.of(context).textTheme.display1.merge(TextStyle(color: Colors.grey[700])),
+          CircleAvatar(
+            backgroundImage: AssetImage('assets/images/img_sample_avatar_2.jpg'),
+          ),
+          _buildWidgetSizedBox(width: 8.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Ditta Amelia',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                ),
+                softWrap: true,
+              ),
+              Text(
+                'December 18, 2019',
+                style: TextStyle(color: Colors.grey[400]),
+              ),
+            ],
           ),
           Expanded(
             child: Wrap(
-              alignment: WrapAlignment.center,
+              alignment: WrapAlignment.end,
               children: <Widget>[
-                Text(
-                  'Home',
-                  style: Theme.of(context).textTheme.title.merge(TextStyle(color: Colors.orangeAccent, fontWeight: FontWeight.bold)),
+                Icon(
+                  FontAwesomeIcons.solidComments,
+                  size: 18.0,
+                  color: Colors.grey[400],
                 ),
-                SizedBox(width: 48.0),
+                _buildWidgetSizedBox(width: 8.0),
                 Text(
-                  'Archive',
-                  style: Theme.of(context).textTheme.title.merge(TextStyle(color: Colors.black45, fontWeight: FontWeight.bold)),
+                  '3',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
                 ),
-                SizedBox(width: 48.0),
-                Text(
-                  'Category',
-                  style: Theme.of(context).textTheme.title.merge(TextStyle(color: Colors.black45, fontWeight: FontWeight.bold)),
-                ),
-                SizedBox(width: 48.0),
-                Text(
-                  'Pages',
-                  style: Theme.of(context).textTheme.title.merge(TextStyle(color: Colors.black45, fontWeight: FontWeight.bold)),
-                ),
-                SizedBox(width: 48.0),
-                Text(
-                  'Contact',
-                  style: Theme.of(context).textTheme.title.merge(TextStyle(color: Colors.black45, fontWeight: FontWeight.bold)),
-                ),
+                _buildWidgetSizedBox(width: 16.0),
               ],
             ),
           ),
-          Icon(
-            FontAwesomeIcons.facebook,
-            size: 20.0,
-            color: Colors.grey[700],
-          ),
-          SizedBox(width: 12.0),
-          Icon(
-            FontAwesomeIcons.twitter,
-            size: 20.0,
-            color: Colors.grey[700],
-          ),
-          SizedBox(width: 12.0),
-          Icon(
-            FontAwesomeIcons.instagram,
-            size: 20.0,
-            color: Colors.grey[700],
-          ),
         ],
+        crossAxisAlignment: CrossAxisAlignment.center,
       ),
+    );
+  }
+
+  Widget _buildWidgetImageCoverItemPost(String image) {
+    return Expanded(
+      flex: 2,
+      child: Image.asset(
+        image,
+        fit: BoxFit.cover,
+        width: widthScreen,
+      ),
+    );
+  }
+
+  Widget _buildWidgetWelcome(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          'assets/images/img_header.png',
+          width: widthScreen,
+          height: heightScreen / 1.5,
+          fit: BoxFit.cover,
+        ),
+        Container(
+          width: widthScreen,
+          height: heightScreen / 1.5,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Welcome to the BengkelRobot',
+                style: Theme.of(context).textTheme.display3.merge(TextStyle(color: Colors.white)),
+              ),
+              _buildWidgetSizedBox(height: 28.0),
+              Wrap(
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.facebook,
+                    color: Colors.white,
+                    size: 36.0,
+                  ),
+                  _buildWidgetSizedBox(width: 72.0),
+                  Icon(
+                    FontAwesomeIcons.twitter,
+                    color: Colors.white,
+                    size: 36.0,
+                  ),
+                  _buildWidgetSizedBox(width: 72.0),
+                  Icon(
+                    FontAwesomeIcons.github,
+                    color: Colors.white,
+                    size: 36.0,
+                  ),
+                  _buildWidgetSizedBox(width: 72.0),
+                  Icon(
+                    FontAwesomeIcons.instagram,
+                    color: Colors.white,
+                    size: 36.0,
+                  ),
+                  _buildWidgetSizedBox(width: 72.0),
+                  Icon(
+                    FontAwesomeIcons.medium,
+                    color: Colors.white,
+                    size: 36.0,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildWidgetSizedBox({double width = 0, double height = 0}) {
+    return SizedBox(
+      width: width,
+      height: height,
     );
   }
 }
