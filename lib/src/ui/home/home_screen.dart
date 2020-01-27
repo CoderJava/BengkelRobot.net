4675f929d054d85ecff4195b48686ba4cc97c659
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2020-01-27 22:38:19 +0700

    Started build development

--- lib/src/ui/home/home_screen.dart
+++ lib/src/ui/home/home_screen.dart
@@ -1,48 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_builder/responsive_builder.dart';
-import 'package:website_bengkel_robot/src/ui/home/home_screen_web.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ResponsiveBuilder(
-      builder: (context, sizingInformation) {
-        if (sizingInformation.deviceScreenType == DeviceScreenType.Mobile) {
-          return Scaffold(
-            body: Container(
-              child: Center(
-                child: Text('Coming soon for Device Mobile'),
-              ),
-            ),
-          );
-        } else if (sizingInformation.deviceScreenType == DeviceScreenType.Desktop) {
-          return HomeScreenWeb();
-        } else if (sizingInformation.deviceScreenType == DeviceScreenType.Tablet) {
-          return Scaffold(
-            body: Container(
-              child: Center(
-                child: Text('Coming soon for Device Tablet'),
-              ),
-            ),
-          );
-        } else if (sizingInformation.deviceScreenType == DeviceScreenType.Watch) {
-          return Scaffold(
-            body: Container(
-              child: Center(
-                child: Text('Coming soon for Device Watch'),
-              ),
-            ),
-          );
-        } else {
-          return Scaffold(
-            body: Container(
-              child: Center(
-                child: Text('Unknown device type'),
-              ),
-            ),
-          );
-        }
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bengkel Robot'),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
     );
   }
 }
