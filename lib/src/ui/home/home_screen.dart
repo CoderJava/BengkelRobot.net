00cb23c558b80f62b3a624c3cff5fb6b2e4e5c33
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2020-01-28 22:05:39 +0700

    Created widget blur background in Home Menu

--- lib/src/ui/home/home_screen.dart
+++ lib/src/ui/home/home_screen.dart
@@ -1,14 +1,38 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double widthScreen = mediaQueryData.size.width;
+    double heightScreen = mediaQueryData.size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Bengkel Robot'),
+      body: Stack(
+        children: <Widget>[
+          _buildWidgetBackground(widthScreen, heightScreen),
+        ],
       ),
-      body: Center(
-        child: Text('Hello World'),
+    );
+  }
+
+  Widget _buildWidgetBackground(double widthScreen, double heightScreen) {
+    return Container(
+      width: widthScreen,
+      height: heightScreen,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/img_sample_1.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+        child: Container(
+          color: Colors.white.withOpacity(0.0),
+        ),
       ),
     );
   }
